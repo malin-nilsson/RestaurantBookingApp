1eeed01ee741f6cd8e49d76ba9ec6bc3aa4f7bef
Malin Nilsson <malinnilsson87@gmail.com>
2022-08-18 15:58:43 +0200

    Getting started on guest details form

--- frontend/src/components/pages/Book.tsx
+++ frontend/src/components/pages/Book.tsx
@@ -1,3 +1,49 @@
+import { FormEvent, useState } from 'react'
+
 export default function Book() {
-  return <div></div>;
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [phone, setPhone] = useState('')
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+  }
+
+  return (
+    <div>
+      <h2>Guest information</h2>
+      <form onSubmit={handleSubmit}>
+        <div></div>
+        <label>Name</label>
+        <input
+          type="text"
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Name"
+          value={name}
+        />
+
+        <div>
+          <label>Email</label>
+          <input
+            type="text"
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email"
+            value={email}
+          />
+        </div>
+
+        <div>
+          <label>Phone</label>
+          <input
+            type="text"
+            onChange={(e) => setPhone(e.target.value)}
+            placeholder="Phone"
+            value={phone}
+          />
+        </div>
+
+        <button>Confirm</button>
+      </form>
+    </div>
+  )
 }
