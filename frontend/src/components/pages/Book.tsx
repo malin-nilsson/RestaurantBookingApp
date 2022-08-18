4ce342b0fa6af59cc860b9a10eb0b2e3d0a9e9d6
Malin Nilsson <malinnilsson87@gmail.com>
2022-08-18 21:53:38 +0200

    Show confirmation when booking is completed

--- frontend/src/components/pages/Book.tsx
+++ frontend/src/components/pages/Book.tsx
@@ -7,6 +7,8 @@ export default function Book() {
   const [email, setEmail] = useState('')
   const [phone, setPhone] = useState('')
   const [error, setError] = useState(false)
+  const [form, setForm] = useState(true)
+  const [confirmation, setConfirmation] = useState(false)
 
   const saveNewGuest = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
@@ -25,45 +27,55 @@ export default function Book() {
     setName('')
     setEmail('')
     setPhone('')
+    setConfirmation(true)
+    setForm(false)
   }
 
   return (
     <div>
       <h2>Guest information</h2>
-      <form onSubmit={saveNewGuest}>
-        <div></div>
-        <label>Name</label>
-        <input
-          type="text"
-          onChange={(e) => setName(e.target.value)}
-          placeholder="Name"
-          value={name}
-        />
-
-        <div>
-          <label>Email</label>
-          <input
-            type="email"
-            onChange={(e) => setEmail(e.target.value)}
-            placeholder="Email"
-            value={email}
-          />
-        </div>
-
-        <div>
-          <label>Phone</label>
+      {form && (
+        <form onSubmit={saveNewGuest}>
+          <div></div>
+          <label>Name</label>
           <input
             type="text"
-            onChange={(e) => setPhone(e.target.value)}
-            placeholder="Phone"
-            value={phone}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="Name"
+            value={name}
           />
+
+          <div>
+            <label>Email</label>
+            <input
+              type="email"
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Email"
+              value={email}
+            />
+          </div>
+
+          <div>
+            <label>Phone</label>
+            <input
+              type="text"
+              onChange={(e) => setPhone(e.target.value)}
+              placeholder="Phone"
+              value={phone}
+            />
+          </div>
+
+          <button>Confirm</button>
+
+          {error && <div>All fields must be filled out.</div>}
+        </form>
+      )}
+
+      {confirmation && (
+        <div>
+          <h3> Your reservation is confirmed.</h3>
         </div>
-
-        <button>Confirm</button>
-
-        {error && <div>All fields must be filled out.</div>}
-      </form>
+      )}
     </div>
   )
 }
