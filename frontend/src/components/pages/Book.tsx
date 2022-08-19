aa06d450aa64d5943840c41d00787e0bc288523b
michael-cwm <michaelcwm@hotmail.com>
2022-08-19 13:39:13 +0200

    commit

--- frontend/src/components/pages/Book.tsx
+++ frontend/src/components/pages/Book.tsx
@@ -6,17 +6,10 @@ import { IGuest } from "../../models/IGuest";
 import axios from "axios";
 import { saveBooking } from "../../services/saveBooking";
 import { limitPastDates } from "../../services/limitDate";
+import React from "react";
 // import { checkAvailability } from "../../services/checkAvailability";
 
 export const Book = () => {
-  const [booking, setBooking] = useState<IBooking>({
-    id: "",
-    date: "",
-    time: "",
-    guest: { name: "", email: "", phone: 0 },
-    amount: 0,
-  });
-
   const [available, setAvailable] = useState<IBooking>({
     id: "",
     date: "",
@@ -24,14 +17,8 @@ export const Book = () => {
     amount: 0,
   });
 
-  // const checkAvailability = () => {
-  //   axios.get("https://localhost:4000/bookings").then((response) => {
-  //     setAvailable(response.data);
-  //     console.log("hej");
-  //   });
-  // };
-
-  const checkAvailability = () => {
+  const checkAvailability = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
     axios.get("https://localhost:4000/bookings").then((response) => {
       setAvailable(response.data);
       console.log(available);
@@ -39,17 +26,16 @@ export const Book = () => {
   };
 
   return (
-    <>
-      <button onClick={checkAvailability}>click me</button>
-      {/* <form onSubmit={() => checkAvailability()}> */}
-      <input type="date" min={limitPastDates()} />
-      <select name="time">
-        <option value="time1">18</option>
-        <option value="time2">21</option>
-      </select>
-      <input type="" min="1" max="6" />
-      <input type="submit" value="Sök bord" />
-      {/* </form> */}
-    </>
+    <div>
+      <form onSubmit={() => checkAvailability(e)}>
+        <input type="date" min={limitPastDates()} />
+        <select name="time">
+          <option value="time1">18</option>
+          <option value="time2">21</option>
+        </select>
+        <input type="" min="1" max="6" />
+        <input type="submit" value="Sök bord" />
+      </form>
+    </div>
   );
 };
