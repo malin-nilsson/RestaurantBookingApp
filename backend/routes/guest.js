bf3368c8063ed92aa3953533b20361f842b31040
Malin Nilsson <malinnilsson87@gmail.com>
2022-08-22 09:50:18 +0200

    Removed unused route

--- backend/routes/guest.js
+++ backend/routes/guest.js
@@ -1,8 +1,4 @@
 const express = require('express')
-const { saveGuest } = require('../controllers/guestController.js')
 const router = express.Router()
-
-// SAVE GUEST
-router.post('/', saveGuest)
 
 module.exports = router
