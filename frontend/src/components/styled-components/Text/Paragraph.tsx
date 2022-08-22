380d645c9c074a604b22ebcf240df2104b97a07b
Malin Nilsson <malinnilsson87@gmail.com>
2022-08-22 09:42:43 +0200

    Styled Text

---
+++ frontend/src/components/styled-components/Text/Paragraph.tsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+interface ITextProps {
+  fontSize?: string
+  padding?: string
+}
+
+export const StyledParagraph = styled.p`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 15px;
+  padding: ${(props: ITextProps) => props.padding || '15px'};
+  margin: 0;
+  font-size: ${(props: ITextProps) => props.fontSize || '1.7rem'};
+  font-weight: 100;
+  text-align: center;
+`
