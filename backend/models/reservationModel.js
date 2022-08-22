ebec852d988621d7590c2f863dd20ec0af7d0a19
Malin Nilsson <malinnilsson87@gmail.com>
2022-08-22 09:46:34 +0200

    Reservation schema

---
+++ backend/models/reservationModel.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const reservationSchema = new Schema({
+  date: { type: String, required: true, default: Date.now },
+  time: { type: String, required: true },
+  amount: { type: Number, required: true },
+  message: { type: String, required: false },
+  guestName: { type: String, required: true },
+  guestEmail: { type: String, required: true },
+  guestPhone: { type: String, required: true },
+})
+
+module.exports = mongoose.model('Reservations', reservationSchema)
---
+++ frontend/src/models/IReservation.ts
@@ -0,0 +1,9 @@
+export interface IReservation {
+  date: string
+  time: string
+  amount: number
+  message?: string
+  guestName: string
+  guestEmail: string
+  guestPhone: string
+}
