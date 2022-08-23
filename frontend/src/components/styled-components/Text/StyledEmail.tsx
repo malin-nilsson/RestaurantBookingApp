11727e3b45082a2a8982fa7f250d4f27b32c5441
michael-cwm <michaelcwm@hotmail.com>
2022-08-23 14:02:26 +0200

    built feature contact page

---
+++ frontend/src/components/styled-components/Text/StyledEmail.tsx
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const StyledEmail = styled.span`
+  font-family: var(--headingfont);
+  font-size: 1.7rem;
+`;
---
+++ frontend/src/components/styled-components/Wrappers/ContactWrapper.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const StyledContactWrapper = styled.div`
+  width: 50%;
+  margin-top: 50px;
+  display: flex;
+  flex-direction: column;
+`;
