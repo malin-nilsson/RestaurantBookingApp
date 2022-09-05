51afcd93f3b4f7d4180294841e58295c6c1eed12
Malin Nilsson <malinnilsson87@gmail.com>
2022-09-05 08:44:20 +0200

    Delete guest information as admin

--- backend/routes/guestRoute.js
+++ backend/routes/guestRoute.js
@@ -1,12 +1,11 @@
 const express = require('express')
-const {
-  getGuests,
-  getBookingsByGuest,
-} = require('../controllers/guestController.js')
+const { getGuests, deleteGuest } = require('../controllers/guestController.js')
 const router = express.Router()
 
 // GET BOOKING
 router.get('/', getGuests)
-router.post('/search', getBookingsByGuest)
+// DELETE BOOKING
+router.delete('/delete/:id', deleteGuest)
+// router.post('/search', getBookingsByGuest)
 
 module.exports = router
