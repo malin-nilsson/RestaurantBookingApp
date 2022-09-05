3f9fe818d292923c834bfbb8fda3e2a17c01f9b3
Oskar Lowitz <19ozman@gmail.com>
2022-09-05 12:21:01 +0200

    removed JWT_SECRET

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,11 +1,10 @@
 const AdminModel = require("../models/adminModel");
 const jwt = require("jsonwebtoken");
-// require("dotenv").config();
 
 // CREATE TOKEN FOR MONGO ID i.e. _id AND CALCULATE MS
 const maxAge = 3 * 24 * 60 * 60;
 const createToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: maxAge });
+  return jwt.sign({ id }, "secret", { expiresIn: maxAge });
 };
 
 // HANDLE ERRORS
